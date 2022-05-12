4671e5f70435a52375c0aad70334567fa0afaaa7
seongolee <seongo0521@gmail.com>
2022-05-12 09:49:36 +0900

    feat: group-anagram 알고리즘
    같은 알파벳들의 모임으로 이루어진 다른 뜻의 단어들을 분리하는 알고리즘.

--- algorithm/group-anagrams.py
+++ algorithm/group-anagrams.py
@@ -1,15 +1,26 @@
 import collections
+from typing import List
+
+"""
+문자열 배열을 받아 애너그램 단위로 그룹핑하라.
+["eat", "tea", "tan", "ate", "nat", "bat"]
+"""
 
 
-def groupAnagrams(strs: list[str]) -> list[list[str]]:
-    anagrams = collections.defaultdict(list)
+class GroupAnagrams:
+    def group_anagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = collections.defaultdict(list)
 
-    for word in strs:
-        # 정렬하여 딕셔너리에 추가
-        anagrams[''.join(sorted(word))].append(word)
-    return list(anagrams.values())
+        # 문자열을 꺼내서 오름 차순으로 정리를 하게 되면 문자열을 문자들의 배열이기때문에 배열로 쪼개지기 때문에
+        # join으로 다시 합쳐주는 과정이 필요하다. 그중 같은 단어들로 이루어 져있으면 append(word)를 하고,
+        # 새로운 단어가 들어오면 defaultdict(list)로 만들어놓았기 때문에 새로운 단어가 키값에 append(word)가 된다..
+        for word in strs:
+            # 정렬하여 딕셔너리에 추가.
+            anagrams[''.join(sorted(word))].append(word)
+
+        return list(anagrams.values())
 
 
-group_list = ['eat', 'tea', 'tan', 'ate', 'nat', 'bat']
-
-print(groupAnagrams(group_list))
+list_a = ["eat", "tea", "tan", "ate", "nat", "bat"]
+anagrams = GroupAnagrams()
+print(anagrams.group_anagrams(list_a))
