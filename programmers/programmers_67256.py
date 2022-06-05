4b233ff64bcef633484f49bef62f01aed4c4d7c4
seongolee <seongo0521@gmail.com>
2022-06-06 08:49:27 +0900

    일정이 부족해 대충 풀었던 문제 다시 풀어보기

--- programmers/programmers_67256.py
+++ programmers/programmers_67256.py
@@ -8,46 +8,40 @@ from typing import List
 def solution(numbers: List[int], hand: str) -> str:
     answer = ''
     keypad = {
-        1: [0, 0], 2: [1, 0], 3: [2, 0],
-        4: [0, 1], 5: [1, 1], 6: [2, 1],
-        7: [0, 2], 8: [1, 2], 9: [2, 2],
-        '*': [0, 3], 0: [1, 3], '#': [2, 3]
+        1: [0, 0], 2: [0, 1], 3: [0, 2],
+        4: [1, 0], 5: [1, 1], 6: [1, 2],
+        7: [2, 0], 8: [2, 1], 9: [2, 2],
+        '*': [3, 0], 0: [3, 1], '#': [3, 2]
     }
 
-    answer = ''
+    # 손의 위치
+    left_hand = keypad['*']
+    right_hand = keypad['#']
 
-    left_p = keypad['*']
-    right_p = keypad['#']
-
-    for n in numbers:
-        if n in [1, 4, 7]:
+    for number in numbers:
+        if number in [1, 4, 7]:
             answer += 'L'
-            left_p = keypad[n]
-        elif n in [3, 6, 9]:
+            left_hand = keypad[number]
+        elif number in [3, 6, 9]:
             answer += 'R'
-            right_p = keypad[n]
+            right_hand = keypad[number]
         else:
-            left_d = 0
-            right_d = 0
+            temp_l = abs(keypad[number][0] - left_hand[0]) + abs(keypad[number][1] - left_hand[1])
+            temp_r = abs(keypad[number][0] - right_hand[0]) + abs(keypad[number][1] - right_hand[1])
 
-            target = keypad[n]
-
-            left_d = abs(left_p[0] - target[0]) + abs(left_p[1] - target[1])
-            right_d = abs(right_p[0] - target[0]) + abs(right_p[1] - target[1])
-
-            if left_d < right_d:
-                answer += 'L'
-                left_p = keypad[n]
-            elif left_d > right_d:
+            if temp_l > temp_r:
                 answer += 'R'
-                right_p = keypad[n]
+                right_hand = keypad[number]
+            elif temp_l < temp_r:
+                answer += 'L'
+                left_hand = keypad[number]
             else:
-                if hand == 'left':
-                    answer += 'L'
-                    left_p = keypad[n]
-                else:
+                if hand == 'right':
                     answer += 'R'
-                    right_p = keypad[n]
+                    right_hand = keypad[number]
+                else:
+                    answer += 'L'
+                    left_hand = keypad[number]
 
     return answer
 
